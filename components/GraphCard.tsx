b0e73d76945b6ef2827ce5cb5e9103aac4daed8f
Tim Headrick <timothyheadrick12@gmail.com>
2023-02-11 09:54:16 +1100

    WIP(): Adding 3d graph support

--- components/GraphCard.tsx
+++ components/GraphCard.tsx
@@ -1,20 +1,44 @@
+import { Data } from "plotly.js";
 import { Card, Text } from "react-native-paper";
+import Plotly from "plotly.js-basic-dist-min";
+import createPlotlyComponent from "react-plotly.js/factory";
+
+const Plot = createPlotlyComponent(Plotly);
+
+function randomValues(num: number, mul: number) {
+  const arr = [];
+  const index = [];
+  for (let i = 0; i < num; i++) {
+    arr.push(Math.random() * mul);
+    index.push(i);
+  }
+  return { index, arr };
+}
 
 const GraphCard = () => {
+  const traces: Data[] = Array(3)
+    .fill(0)
+    .map((_, i) => {
+      const { index, arr } = randomValues(20, 3);
+      return {
+        x: Array(20).fill(i),
+        y: index,
+        z: arr,
+        type: "scatter3d",
+        mode: "lines",
+      };
+    });
+
   return (
     <Card style={{ height: 350, margin: 10 }} mode="elevated" elevation={2}>
-      <Text
-        style={{
-          transform: [
-            { rotate: "45deg" },
-            { translateX: 190 },
-            { translateY: 120 },
-          ],
+      <Plot
+        data={traces}
+        layout={{
+          width: 900,
+          height: 800,
+          title: `Simple 3D Scatter`,
         }}
-        variant="headlineLarge"
-      >
-        Your Graph Here!
-      </Text>
+      />
     </Card>
   );
 };
