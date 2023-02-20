4af5a46a4328b5242ab694dd432d24aeb90fadfc
Tim Headrick <timothyheadrick12@gmail.com>
2023-02-20 14:26:16 +1100

    WIP(GraphCard): Initial setup for three.js based 3d

--- components/GraphCard.tsx
+++ components/GraphCard.tsx
@@ -1,38 +1,72 @@
-import { Data } from "plotly.js";
+import {
+  Scene,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  BoxGeometry,
+} from "three";
+import ExpoTHREE, { Renderer } from "expo-three";
+import { ExpoWebGLRenderingContext, GLView } from "expo-gl";
 import { Card, Text } from "react-native-paper";
-import Plotly from "react-native-plotly";
-
-function randomValues(num: number, mul: number) {
-  const arr = [];
-  const index = [];
-  for (let i = 0; i < num; i++) {
-    arr.push(Math.random() * mul);
-    index.push(i);
-  }
-  return { index, arr };
-}
 
 const GraphCard = () => {
-  const traces: Data[] = Array(3)
-    .fill(0)
-    .map((_, i) => {
-      const { index, arr } = randomValues(20, 3);
-      return {
-        x: randomValues(20, 3).arr,
-        y: randomValues(20, 3).arr,
-        z: arr,
-        type: "scatter3d",
-        mode: "markers",
-      };
+  const onContextCreate = async (gl: any) => {
+    const scene = new Scene();
+    const camera = new PerspectiveCamera(
+      75,
+      gl.drawingBufferWidth / gl.drawingBufferHeight,
+      0.1,
+      1000
+    );
+    gl.canvas = {
+      width: gl.drawingBufferWidth,
+      height: gl.drawingBufferHeight,
+    };
+
+    // set camera position away from cube
+    camera.position.z = 2;
+
+    const renderer = new Renderer({ gl });
+    // set size of buffer to be equal to drawing buffer width
+    renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+
+    // create cube
+    // define geometry
+    const geometry = new BoxGeometry(1, 1, 1);
+    const material = new MeshBasicMaterial({
+      color: "cyan",
     });
 
+    const cube = new Mesh(geometry, material);
+
+    // add cube to scene
+    scene.add(cube);
+
+    // create render function
+    const render = () => {
+      requestAnimationFrame(render);
+      // create rotate functionality
+      // rotate around x axis
+      cube.rotation.x += 0.01;
+
+      // rotate around y axis
+      cube.rotation.y += 0.01;
+
+      renderer.render(scene, camera);
+      gl.endFrameEXP();
+    };
+
+    // call render
+    render();
+  };
+
   return (
-    <Plotly
-      data={traces}
-      layout={{
-        title: `Simple 3D Scatter`,
-      }}
-    />
+    <Card style={{ height: 350, margin: 10 }} mode="elevated" elevation={2}>
+      <GLView
+        onContextCreate={onContextCreate}
+        style={{ width: "100%", height: "100%" }}
+      />
+    </Card>
   );
 };
 
