53ad28db80826c545497aa08f84950d81cec92c1
Prosperf <timothyheadrick12@gmail.com>
2023-03-23 17:41:54 +1100

    FEAT(): QoL app features

--- components/WorkoutCard.tsx
+++ components/WorkoutCard.tsx
@@ -1,9 +1,16 @@
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { Card, IconButton } from "react-native-paper";
+import { useState } from "react";
+import { Appbar, Card, IconButton } from "react-native-paper";
+import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { NavigationProps } from "../App";
-import { WorkoutData } from "../reducers/workoutsReducer";
+import { setEditMode } from "../reducers/editModeReducer";
+import {
+  setWorkoutSelected,
+  unselectAll,
+  WorkoutData,
+} from "../reducers/workoutsReducer";
 import { RootState } from "../store";
 
 interface WorkoutCardProps {
@@ -13,6 +20,9 @@ interface WorkoutCardProps {
 
 const WorkoutCard = (props: WorkoutCardProps) => {
   const theme = useSelector((state: RootState) => state.theme);
+  const editMode = useSelector((state: RootState) => state.editMode);
+  const [selected, setSelected] = useState(false);
+  const dispatch = useDispatch();
 
   return (
     <Card
@@ -22,16 +32,42 @@ const WorkoutCard = (props: WorkoutCardProps) => {
         marginVertical: 2,
         marginHorizontal: 10,
       }}
-      onLongPress={() => null}
-      onPress={() =>
-        props.navigation.navigate("Workout", {
-          workout: props.workout,
-        })
-      }
+      onLongPress={() => {
+        if (editMode) {
+          dispatch(unselectAll());
+        }
+        dispatch(setEditMode(!editMode));
+      }}
+      onPress={() => {
+        if (editMode) {
+          dispatch(
+            setWorkoutSelected({
+              id: props.workout.id!,
+              selected: !props.workout.selected!,
+            })
+          );
+        } else {
+          props.navigation.navigate("Workout", {
+            workout: props.workout,
+          });
+        }
+      }}
       delayLongPress={500}
     >
       <Card.Title
         title={props.workout.name}
+        left={
+          editMode
+            ? () => (
+                <IconButton
+                  icon={
+                    props.workout.selected ? "check-circle" : "circle-outline"
+                  }
+                  iconColor={theme.theme.colors.primary}
+                />
+              )
+            : undefined
+        }
         right={(props) => (
           <IconButton
             icon={{ source: "chevron-right", direction: "ltr" }}
