a69445bfca4456e25531040e660cc29d5a910f59
Prosperf <timothyheadrick12@gmail.com>
2023-03-24 07:02:36 +1100

    FIX(): Added scroll to workout

--- screens/WorkoutScreen.tsx
+++ screens/WorkoutScreen.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { View, TextInput } from "react-native";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+import {
+  GestureHandlerRootView,
+  ScrollView,
+} from "react-native-gesture-handler";
 import { Text } from "react-native-paper";
 import WebView from "react-native-webview";
 import { useDispatch } from "react-redux";
@@ -34,47 +37,51 @@ const WorkoutString = ({ route }: any) => {
   const theme = useSelector((state: RootState) => state.theme);
 
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <View
-        style={{
-          justifyContent: "center",
-          alignItems: "center",
-          width: "100%",
-          aspectRatio: 3 / 2,
-        }}
-      >
-        {editMode ? (
-          <TextInput
-            style={{
-              color: theme.theme.colors.primary,
-              fontFamily: theme.theme.fonts.displayLarge.fontFamily,
-              fontSize: theme.theme.fonts.displayLarge.fontSize,
-              fontWeight: theme.theme.fonts.displayLarge.fontWeight,
-              letterSpacing: theme.theme.fonts.displayLarge.letterSpacing,
-              lineHeight: theme.theme.fonts.displayLarge.lineHeight,
-            }}
-            onSubmitEditing={() => {
-              dispatch(updateWorkout({ ...route.params.workout, name: title }));
-              dispatch(setEditMode(false));
-            }}
-            onChangeText={setTitle}
-            value={title}
-          />
-        ) : (
-          <Text
-            onLongPress={() => dispatch(setEditMode(!editMode))}
-            variant="displayLarge"
-          >
-            {title}
-          </Text>
-        )}
-        <Text variant="headlineMedium">{`${
-          months[workoutDate.getMonth()]
-        } ${workoutDate.getDate()}, ${workoutDate.getFullYear()}`}</Text>
-      </View>
-      <StatsCard workout={route.params.workout} />
-      <GraphCard />
-    </GestureHandlerRootView>
+    <ScrollView>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <View
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100%",
+            aspectRatio: 3 / 2,
+          }}
+        >
+          {editMode ? (
+            <TextInput
+              style={{
+                color: theme.theme.colors.primary,
+                fontFamily: theme.theme.fonts.displayLarge.fontFamily,
+                fontSize: theme.theme.fonts.displayLarge.fontSize,
+                fontWeight: theme.theme.fonts.displayLarge.fontWeight,
+                letterSpacing: theme.theme.fonts.displayLarge.letterSpacing,
+                lineHeight: theme.theme.fonts.displayLarge.lineHeight,
+              }}
+              onSubmitEditing={() => {
+                dispatch(
+                  updateWorkout({ ...route.params.workout, name: title })
+                );
+                dispatch(setEditMode(false));
+              }}
+              onChangeText={setTitle}
+              value={title}
+            />
+          ) : (
+            <Text
+              onLongPress={() => dispatch(setEditMode(!editMode))}
+              variant="displayLarge"
+            >
+              {title}
+            </Text>
+          )}
+          <Text variant="headlineMedium">{`${
+            months[workoutDate.getMonth()]
+          } ${workoutDate.getDate()}, ${workoutDate.getFullYear()}`}</Text>
+        </View>
+        <StatsCard workout={route.params.workout} />
+        <GraphCard />
+      </GestureHandlerRootView>
+    </ScrollView>
   );
 };
 
